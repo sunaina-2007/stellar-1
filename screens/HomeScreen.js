aa6204e284384ba44e941f64ed7e7cf387fd6ac3
sunaina-2007 <80744244+sunaina-2007@users.noreply.github.com>
2021-09-08 08:41:34 +0530

    Add files via upload

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class HomeScreen extends Component {
+    render(){
+    return(
+    <View style = {{flex: 1,justifyContent: 'center',alignItems: 'center'}}>    
+    <Text>Home Screen</Text>
+    </View>
+    )    
+    }    
+    }
